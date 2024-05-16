aa650bdbb5a4c4617a7314fcc5ece61b45669db1
MohdShoaib530 <mohdshoaib91530@gmail.com>
2024-05-16 12:38:11 +0530

    Remove Prettier configuration files and update asyncHandler.js

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,6 +1,6 @@
 const asyncHandler = (fn) => {
-    return (req,res,next) => {
-        Promise.resolve(fn(req,res,next)).catch((error) => next(error));
+    return (req, res, next) => {
+        Promise.resolve(fn(req, res, next)).catch((error) => next(error));
     };
 };
 
